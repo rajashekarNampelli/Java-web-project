aebbc16d1f761b024433a6616cb9783d684920e6
rnampell <rajashekar.nampelli@gmail.com>
2017-02-10 01:08:18 -0600

    Added Maven project.

---
+++ JavaMongo/src/main/java/com/asus/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package com.asus.dao;
+
+import com.mongodb.MongoClient;
+
+public class EmployeeDao {
+public static void main(String[] args) {
+	MongoClient  mongo = new MongoClient("localhost", 27017);
+	
+}
+	
+	
+}
